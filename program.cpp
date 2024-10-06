3aa15dc1ba2d52d1807e067bd357531230675c95
iqlimajuliana <iqlimajuliana41@gmail.com>
2024-10-06 10:24:02 +0700

    Update program.cpp

--- program.cpp
+++ program.cpp
@@ -1,1 +1,11 @@
-tutor
+#include <iostream >
+using namespace std; 
+void Menu() {
+    cout << "=============================" << endl;
+    cout << "      MENU LAYANAN KENDARAAN      " << endl;
+    cout << "=============================" << endl;
+    cout << "1. Service ringan (Harga: Rp.200.000)" << endl;
+    cout << "2. Service berkala (Harga: Rp.400.000)" << endl;
+    cout << "3. Service berat (Harga: Rp.600.000)" << endl;
+    cout << "=============================" << endl;
+}
