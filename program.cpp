613c7c4c69c427dec66ccb44a12e19ef0ba9392e
MZhafiqAindillahSani <m.zhafiqaindillahsani12@gmail.com>
2024-10-06 10:45:39 +0700

    Update program.cpp

--- program.cpp
+++ program.cpp
@@ -1,11 +1,27 @@
-#include <iostream >
-using namespace std; 
-void Menu() {
-    cout << "=============================" << endl;
-    cout << "      MENU LAYANAN KENDARAAN      " << endl;
-    cout << "=============================" << endl;
-    cout << "1. Service ringan (Harga: Rp.200.000)" << endl;
-    cout << "2. Service berkala (Harga: Rp.400.000)" << endl;
-    cout << "3. Service berat (Harga: Rp.600.000)" << endl;
-    cout << "=============================" << endl;
+#include <iostream>
+using namespace std;
+
+
+int harga(int bulan) {
+    if(bulan < 2) {
+       retur  200000;
+    }
+    else if (bulan <= 4) {
+         return 400000;
+    }
+    else {
+         return 600000;
+    }
 }
+void ask(int bulan) {
+     string jawab;
+     int price = harga(bulan);
+     cout << "Apakah ada kerusakan di kendaraan anda? (ada / tidak) : ";
+     cin>>jawab;
+
+
+
+
+    if (jawab == "ada"){
+       cout<< "Service yang disarankan : Service berat" << endl;
+       cout<< "Total Biaya: 600.000" << endl;
